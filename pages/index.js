93c91666d7244320363ea3109ea942cd3307214b
ghulam03 <ghulam8905@gmail.com>
2022-11-10 21:13:20 +0530

    add name

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ function HomePage(props) {
   return (
     <Fragment>
       <Head>
-        <title>Max' Blog</title>
+        <title>Ghulam Blog</title>
         <meta
           name='description'
           content='I post about programming and web development.'
